e60e5f9b4cfe13806798f3645e27b0ee27405685
TyBeas <beasl110@morris.umn.edu>
2025-01-29 21:07:57 -0600

    Fixing my chart file

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ## Regex Exercise 0
-awk -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 ## Regex Exercise 1
 awk 'match($0, /([A-Z][a-zA-Z]+)\..+is\s([a-z]+)\./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n"}' < r1_input.txt > r1_output.txt
 
 ## Regex Exercise 2
-awk -E 's/\* sandwich with ([a-zA-Z.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
+sed -E 's/\* sandwich with ([a-zA-Z.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
