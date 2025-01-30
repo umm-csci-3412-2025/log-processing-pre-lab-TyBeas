06db6a7a021a53185ddbfcd03806892600e6425a
TyBeas <beasl110@morris.umn.edu>
2025-01-29 20:59:08 -0600

    Making regex.sh

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-textFile=$1 #Stores the content file as an argument
-headerAndFooter=$2 #Stores the files of the headers and the footers as an argument
-fileDestination=$3 #Stores the detination of the wrapped contents as an argument
+textFile=$1 #Stores the text file as an argument
+headerAndFooter=$2 #Stores the files that contain the header and footer as an argument
+fileDestination=$3 #Stores the destination file as an argument
 
-# Use concatnate to put the textFile in between the header and the footer
-# Subsequentally putting the result into the destination file
+# Use concatnate to combine the header and footer files with the text file
+# Subsequentally output the result to the destination file
 cat "${headerAndFooter}_header.html" "$textFile" "${headerAndFooter}_footer.html" > "$fileDestination"
